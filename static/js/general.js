ea0e30401ac9c3f603afa587055e4d5775540a90
joshuagruenstein <joshuagruenstein@gmail.com>
2016-05-29 21:39:09 -0400

    universal search in menu

--- static/js/general.js
+++ static/js/general.js
@@ -9,3 +9,30 @@ $('#accountBtn').popup({
     hoverable: true,
     position : 'bottom center'
 });
+
+
+/* Look up categories in semantic search.
+ * For some reason they can't run locally,
+ * but we'll use them with our API to
+ * seperate users, problems, and blog posts.
+ * For the moment though, they're together.
+ */
+
+var dummySearchContent = [
+    {
+      title: 'Traveling Salesman Problem',
+      description: 'Find the optimal route through a set of 500 points in 3D space.'
+    },
+    {
+      title: 'Tron: A Postmortem',
+      description: 'by Jake Sanders'
+    },
+    {
+      title: 'Joshua Gruenstein',
+      description: 'Horace Mann'
+    }
+];
+
+$('.ui.search').search({
+    source: dummySearchContent
+});
