6bde2c144cb071c24e20a1fc9ffd11a85aee551c
joshuagruenstein <joshuagruenstein@gmail.com>
2016-05-29 23:43:15 -0400

    table and player feed

--- static/js/general.js
+++ static/js/general.js
@@ -36,3 +36,14 @@ var dummySearchContent = [
 $('.ui.search').search({
     source: dummySearchContent
 });
+
+var rows = document.querySelectorAll(".playerRow");
+for (var i=0; i<rows.length; i++) {
+    var className = rows[i].className;
+    className = className.substr(className.indexOf(' ')+1);
+    $('.playerRow.' + className).popup({
+        popup:$('.gamePopup.' + className),
+        hoverable:true,
+        position : 'bottom right'
+    });
+}
