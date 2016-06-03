42548305b4ed5be7ac6b087fc2097fe34ba49006
joshuagruenstein <joshuagruenstein@gmail.com>
2016-06-02 22:18:45 -0400

    react-ify feed

--- static/js/main.js
+++ static/js/main.js
@@ -15,7 +15,7 @@ var PlayerRow = React.createClass({
     render: function() {
         var user = this.props.data.user;
         return (
-            <tr class={'playerRow' + user.id}>
+            <tr className={'playerRow ' + user.id}>
                 <td><b>{this.props.data.rank}</b></td>
                 <td><a href={'/user/' + user.username}>{user.name}</a></td>
                 <td>{user.school}</td>
@@ -45,12 +45,73 @@ var Leaderboard = React.createClass({
     }
 });
 
-var data = [
+var FeedEvent = React.createClass({
+    render: function() {
+        return (
+            <div className="event">
+                <div className="label">
+                    <i className={this.props.data.type === "upload" ? "file archive outline icon" : "game icon"}></i>
+                </div>
+                <div className="content summary">
+                    <div className="summary">
+                        { this.props.data.event }
+                        <div className="date">
+                            { timeSince(this.props.data.time) + " ago" }
+                        </div>
+                    </div>
+                </div>
+            </div>
+        );
+    }
+});
+
+var Feed = React.createClass({
+    render: function() {
+        return (
+            <div className={"ui flowing popup gamePopup " + this.props.data.id}>
+                <div className="ui feed">
+                    {this.props.data.events.map(function(feedEvent) {
+                        return <FeedEvent data={feedEvent} />
+                    })}
+                </div>
+            </div>
+        );
+    }
+});
+
+var Feeds = React.createClass({
+    render: function() {
+        return (
+            <div>
+                {this.props.data.map(function(feed) {
+                        return <Feed data={feed} />
+                })}
+            </div>
+        );
+    }
+});
+
+
+var leaderData = [
     {user: {id: 1, username: "joshuagruenstein", name: "Joshua Gruenstein", school: "Horace Mann"}, rank: 1, score: 44},
     {user: {id: 2, username: "truell20", name: "Michael Truell", school: "Horace Mann"}, rank: 2, score: 33}
-]
+];
+
+var feedData = [
+    {id: 1, events: [{type: "upload", event: "New bot uploaded", time: new Date(new Date().getTime()-300000)},
+                     {type: "game", event: "Won against Henry Hunt", time: new Date(new Date().getTime()-600000)}]},
+    {id: 2, events: [{type: "upload", event: "New bot uploaded", time: new Date(new Date().getTime()-300000)},
+                     {type: "game", event: "Won against Joshua Gruenstein", time: new Date(new Date().getTime()-600000)}]}
+];
 
 ReactDOM.render(
-    <Leaderboard data={data} />,
+    <Leaderboard data={leaderData} />,
     document.getElementById('leaderBoard')
 );
+
+ReactDOM.render(
+    <Feeds data={feedData} />,
+    document.getElementById('feedsBox')
+);
+
+linkPopups();
