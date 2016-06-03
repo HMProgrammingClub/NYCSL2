416d0823e632f92a7656bbbd85e11191cfc96833
joshuagruenstein <joshuagruenstein@gmail.com>
2016-06-03 14:27:01 -0400

    react-ify navbar + clean up

--- static/js/general.js
+++ static/js/general.js
@@ -1,46 +1,214 @@
-$("#sidebarBtn").click(function(e) {
-    e.preventDefault();
-    $('.ui.sidebar').sidebar('toggle');
-    return false;
-});
+var Sidebar = React.createClass({
+    componentDidMount: function() {
+        /* Look up categories in semantic search.
+         * For some reason they can't run locally,
+         * but we'll use them with our API to
+         * seperate users, problems, and blog posts.
+         * For the moment though, they're together.
+         */
 
-$("#loginBtn").click(function(e) {
-    e.preventDefault();
-    $('.ui.modal').modal('show');
-    return false;
-});
+        var dummySearchContent = [
+            {
+              title: 'Traveling Salesman Problem',
+              description: 'Find the optimal route through a set of 500 points in 3D space.'
+            },
+            {
+              title: 'Tron: A Postmortem',
+              description: 'by Jake Sanders'
+            },
+            {
+              title: 'Joshua Gruenstein',
+              description: 'Horace Mann'
+            }
+        ];
 
-$('#accountBtn').popup({
-    popup: '.special.popup',
-    hoverable: true,
-    position : 'bottom center'
-});
-
-
-/* Look up categories in semantic search.
- * For some reason they can't run locally,
- * but we'll use them with our API to
- * seperate users, problems, and blog posts.
- * For the moment though, they're together.
- */
-
-var dummySearchContent = [
-    {
-      title: 'Traveling Salesman Problem',
-      description: 'Find the optimal route through a set of 500 points in 3D space.'
+        $('.ui.search').search({
+            source: dummySearchContent
+        });
     },
-    {
-      title: 'Tron: A Postmortem',
-      description: 'by Jake Sanders'
+    gameList: function(games, active) {
+        var season = -1;
+        return games.map(function(game) {
+            if (game.season != season) {
+                season = game.season;
+                return (
+                    <div key={game.id}>
+                        <div className="ui horizontal divider inverted fitted">
+                            {"Season " + game.season}
+                        </div>
+                        <a href={"/?game="+game.id} className={"item" + (active===game.id?" active":"")}>{game.name}</a>
+                    </div>
+                );
+            } else return (
+                <a href={"/?game="+game.id} className={"item" + (active===game.id?" active":"")} key={game.id}>{game.name}</a>
+            );
+        });
     },
-    {
-      title: 'Joshua Gruenstein',
-      description: 'Horace Mann'
+    render: function() {
+        return (
+            <div className="ui sidebar inverted vertical menu">
+                <div className="item">
+                    <div className="ui fluid category search">
+                        <div className="ui icon input fluid">
+                            <input className="prompt" type="text" placeholder="Search..." />
+                            <i className="search icon"></i>
+                        </div>
+                        <div className="results"></div>
+                    </div>
+                </div>
+                <div className="item">
+                    <div className="header">Games</div>
+                    <div className="menu">
+                        { this.gameList(this.props.games, this.props.active) }
+                    </div>
+                </div>
+                <a className={"item" + (this.props.active==="users"?" active":"")}>
+                    Users
+                </a>
+                <a className={"item" + (this.props.active==="blog"?" active":"")}>
+                    Blog
+                </a>
+                <div className="item">
+                    <div className="header">About</div>
+                    <div className="content" style={{fontSize:"0.9em",lineHeight:"1.4em"}}>NYCSL is an inter-mural programming competition for highschool students in the New York area.  Each month-ish a new problem is posted, and participants are challenged to create the optimal solution.  Challenges can be games, algorithmic problems, or something else entirely.  The top ranked students for each challenge receive prizes, and are invited to attend the NYCSL Championship at Horace Mann in the summer.</div>
+                </div>
+            </div>
+        );
     }
+});
+
+var NavBar = React.createClass({
+    componentDidMount: function() {
+        $("#sidebarBtn").click(function(e) {
+            e.preventDefault();
+            $('.ui.sidebar').sidebar('toggle');
+            return false;
+        });
+
+        $("#loginBtn").click(function(e) {
+            e.preventDefault();
+            $('.ui.modal').modal('show');
+            return false;
+        });
+
+        $('#accountBtn').popup({
+            popup: '.special.popup',
+            hoverable: true,
+            position : 'bottom center'
+        });
+
+        $('.ui.dropdown').dropdown();
+    },
+    signInModal: function() {
+        return (
+            <div className="ui modal">
+                <div className="header">
+                    Login with Github
+                </div>
+                <div className="image content">
+                    <div className="description">
+                        <div className="ui header">Click the button below to login with Github.</div>
+                        <p>If you don't already have an account linked to your Github profile, that's ok. We@apos;ll automatically detect it and help you get things set up.</p>
+                    </div>
+                </div>
+                <div className="actions">
+                    <button className="ui button fluid">
+                        <i className="github icon"></i>
+                        Connect to Github
+                    </button>
+                </div>
+            </div>
+        );
+    },
+    signUpModal: function() {
+        return (
+            <div className="ui modal">
+                <div className="header">
+                    Signup with Github
+                </div>
+                <div className="image content"><div className="description">
+                    <div className="ui form">
+                        <div className="field">
+                            <label>School</label>
+                            <select className="ui fluid search dropdown selection">
+                                <option value="">Select School</option>
+                                <option value="HM">Horace Mann School</option>
+                                <option value="DA">Dalton School</option>
+                                <option value="ST">Stuyvesant</option>
+                            </select>
+                        </div>
+                    </div>
+                </div></div>
+                <div className="actions">
+                    <button className="ui button fluid">
+                        Create Account
+                    </button>
+                </div>
+            </div>
+        );
+    },
+    loggedInMenu: function(githubID) {
+        return (
+            <div className="right menu">
+                <a className="item">
+                    <i className="upload icon"></i>
+                    Submit
+                </a>
+                <a className="item">
+                    <img className="ui mini circular image" id="accountBtn" src={"https://avatars0.githubusercontent.com/u/"+githubID+"?v=3&s=100"} />
+                </a>
+                <div className="ui special popup vertical menu" style={{width:"110px", padding:"5px 0px"}}>
+                    <a className="item">
+                        <i className="user icon"></i>
+                        Profile
+                    </a>
+                    <a className="item">
+                        <i className="sign out icon"></i>
+                        Logout
+                    </a>
+                </div>
+            </div>
+        );
+    },
+    loggedOutMenu: function() {
+        return (
+            <div className="right menu">
+                <a className="item" id="loginBtn">
+                    <i className="sign in icon"></i>
+                    Login with Github
+                </a>
+            </div>
+        );
+    },
+    render: function() {
+        return (
+            <div className="ui secondary menu segment">
+                <a className="item" href="/">
+                    <b>NYCSL</b>
+                </a>
+                <a className="item" id="sidebarBtn">
+                    <i className="sidebar icon"></i> Menu
+                </a>
+                { this.props.logState ? this.loggedInMenu(this.props.githubID) : this.loggedOutMenu() }
+            </div>
+        );
+    }
+});
+
+
+var games = [
+    { id:"ST", name:"Steiner Tree", season:0 },
+    { id:"TR", name:"Tron", season:0 },
+    { id:"RM", name:"Roommate", season:0 },
+    { id:"TS", name:"Traveling Salesman", season:0 }
 ];
 
-$('.ui.search').search({
-    source: dummySearchContent
-});
+ReactDOM.render(
+    <Sidebar games={games} active="ST" />,
+    document.getElementById('sidebarBox')
+);
 
-$('.ui.dropdown').dropdown();
+ReactDOM.render(
+    <NavBar githubID="7736334" logState={true} />,
+    document.getElementById('navbarBox')
+);
--- static/js/main.js
+++ static/js/main.js
@@ -1,43 +1,3 @@
-function linkPopups() {
-    var rows = document.querySelectorAll(".playerRow");
-    for (var i=0; i<rows.length; i++) {
-        var className = rows[i].className;
-        className = className.substr(className.indexOf(' ')+1);
-        $('.playerRow.' + className).popup({
-            popup:$('.gamePopup.' + className),
-            hoverable:true,
-            position : 'bottom right'
-        });
-    }
-}
-
-var Leaderboard = React.createClass({
-    render: function() {
-        return (
-            <table className="ui table unstackable">
-                <thead><tr>
-                    <th>Rank</th>
-                    <th>Name</th>
-                    <th>School</th>
-                    <th>Score</th>
-                </tr></thead>
-                <tbody>
-                    {this.props.rows.map(function(row) {
-                        return (
-                            <tr className={'playerRow ' +row.user.id} key={row.rank}>
-                                <td><b>{row.rank}</b></td>
-                                <td><a href={'/user/' + row.user.username}>{row.user.name}</a></td>
-                                <td>{row.user.school}</td>
-                                <td>{row.score}</td>
-                            </tr>
-                        );
-                    })}
-                </tbody>
-            </table>
-        );
-    }
-});
-
 var Feed = React.createClass({
     render: function() {
         return (
@@ -66,18 +26,51 @@ var Feed = React.createClass({
     }
 });
 
-var Feeds = React.createClass({
+
+var Leaderboard = React.createClass({
+    componentDidMount: function() {
+        // link rows and popups
+        var rows = document.querySelectorAll(".playerRow");
+        for (var i=0; i<rows.length; i++) {
+            var className = rows[i].className;
+            className = className.substr(className.indexOf(' ')+1);
+            $('.playerRow.' + className).popup({
+                popup:$('.gamePopup.' + className),
+                hoverable:true,
+                position : 'bottom right'
+            });
+        }
+    },
     render: function() {
         return (
-            <div>
+            <div className="leaderboard">
+                <table className="ui table unstackable">
+                    <thead><tr>
+                        <th>Rank</th>
+                        <th>Name</th>
+                        <th>School</th>
+                        <th>Score</th>
+                    </tr></thead>
+                    <tbody>
+                        {this.props.rows.map(function(row) {
+                            return (
+                                <tr className={'playerRow ' +row.user.id} key={row.rank}>
+                                    <td><b>{row.rank}</b></td>
+                                    <td><a href={'/user/' + row.user.username}>{row.user.name}</a></td>
+                                    <td>{row.user.school}</td>
+                                    <td>{row.score}</td>
+                                </tr>
+                            );
+                        })}
+                    </tbody>
+                </table>
                 {this.props.feeds.map(function(feed) {
-                        return <Feed events={feed.events} id={feed.id} key={feed.id} />
+                    return <Feed events={feed.events} id={feed.id} key={feed.id} />
                 })}
             </div>
         );
     }
 });
-
 
 var leaderData = [
     {user: {id: 1, username: "joshuagruenstein", name: "Joshua Gruenstein", school: "Horace Mann"}, rank: 1, score: 44},
@@ -91,81 +84,7 @@ var feedData = [
                      {type: "game", event: "Won against Joshua Gruenstein", time: new Date(new Date().getTime()-600000)}]}
 ];
 
-var games = [
-    { id:"ST", name:"Steiner Tree", season:0 },
-    { id:"TR", name:"Tron", season:0 },
-    { id:"RM", name:"Roommate", season:0 },
-    { id:"TS", name:"Traveling Salesman", season:0 }
-];
-
 ReactDOM.render(
-    <Leaderboard rows={leaderData} />,
+    <Leaderboard rows={leaderData} feeds={feedData} />,
     document.getElementById('leaderBoard')
 );
-
-ReactDOM.render(
-    <Feeds feeds={feedData} />,
-    document.getElementById('feedsBox')
-);
-
-var Sidebar = React.createClass({
-    gameList: function(games, active) {
-        var season = -1;
-        var newGames = games.map(function(game) {
-            if (game.season != season) {
-                season = game.season;
-                return (
-                    <div key={game.id}>
-                        <div className="ui horizontal divider inverted fitted">
-                            {"Season " + game.season}
-                        </div>
-                        <a href={"/?game="+game.id} className={"item" + (active===game.id?" active":"")}>{game.name}</a>
-                    </div>
-                );
-            } else return (
-                <a href={"/?game="+game.id} className={"item" + (active===game.id?" active":"")} key={game.id}>{game.name}</a>
-            );
-        });
-
-        return newGames;
-    },
-    render: function() {
-        return (
-            <div className="ui sidebar inverted vertical menu">
-                <div className="item">
-                    <div className="ui fluid category search">
-                        <div className="ui icon input fluid">
-                            <input className="prompt" type="text" placeholder="Search..." />
-                            <i className="search icon"></i>
-                        </div>
-                        <div className="results"></div>
-                    </div>
-                </div>
-                <div className="item">
-                    <div className="header">Games</div>
-                    <div className="menu">
-                        { this.gameList(this.props.games, this.props.active) }
-                    </div>
-                </div>
-                <a className={"item" + (this.props.active==="users"?" active":"")}>
-                    Users
-                </a>
-                <a className={"item" + (this.props.active==="blog"?" active":"")}>
-                    Blog
-                </a>
-                <div className="item">
-                    <div className="header">About</div>
-                    <div className="content" style={{fontSize:"0.9em",lineHeight:"1.4em"}}>NYCSL is an inter-mural programming competition for highschool students in the New York area.  Each month-ish a new problem is posted, and participants are challenged to create the optimal solution.  Challenges can be games, algorithmic problems, or something else entirely.  The top ranked students for each challenge receive prizes, and are invited to attend the NYCSL Championship at Horace Mann in the summer.</div>
-                </div>
-            </div>
-        );
-    }
-});
-
-
-ReactDOM.render(
-    <Sidebar games={games} active="ST" />,
-    document.getElementById('sidebarBox')
-);
-
-linkPopups();
