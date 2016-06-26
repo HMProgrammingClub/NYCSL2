61cf6af1698689a7e9322d5242640eac15fd6c9d
Michael Truell <michael_truell@horacemann.org>
2016-06-26 15:27:12 -0400

    Redact submission endpoint

--- backend/main.py
+++ backend/main.py
@@ -117,66 +117,6 @@ class ProblemAPI(Resource):
 			abort(404)
 		return jsonify({"result": True})
 
-class SubmissionListAPI(Resource):
-	def __init__(self):
-		self.parser = reqparse.RequestParser()
-		self.parser.add_argument("abbreviation", type=str, required=True, location="json")
-		self.parser.add_argument("isAscending", type=bool, required=True, location="json")
-		self.parser.add_argument("name", type=str, required=True, location="json")
-		self.parser.add_argument("description", type=str, required=True, location="json")
-		super(ProblemListAPI, self).__init__()
-
-	def get(self):
-		return jsonify([a for a in db.problem.find({})])
-
-	def post(self):
-		problem = self.parser.parse_args()
-
-		db.problem.insert_one(problem)
-
-		return jsonify(problem, status=201)
-
-class SubmissionAPI(Resource):
-	def __init__(self):
-		self.parser = reqparse.RequestParser()
-		self.parser.add_argument("abbreviation", type=str, location="json")
-		self.parser.add_argument("isAscending", type=bool, location="json")
-		self.parser.add_argument("name", type=str, location="json")
-		self.parser.add_argument("description", type=str, location="json")
-		super(ProblemAPI, self).__init__()
-
-	def get(self, problemID):
-		try:
-			problem = db.problem.find_one({"_id": ObjectId(problemID)})
-		except:
-			abort(404)
-		if problem is None:
-			abort(404)
-		return jsonify(problem)
-
-	def put(self, problemID):
-		try:
-			problem = db.problem.find_one({"_id": ObjectId(problemID)})
-		except:
-			abort(404)
-		if problem is None:
-			abort(404)
-
-		args = self.parser.parse_args()
-		for k, v in args.items():
-			if v is not None:
-				problem[k] = v
-
-		db.problem.save(problem)
-		return jsonify(problem)
-
-	def delete(self, problemID):
-		result = db.problem.delete_one({"_id": ObjectId(problemID)})
-		if result.deleted_count < 1:
-			abort(404)
-		return jsonify({"result": True})
-
-
 class UserListAPI(Resource):
 	def __init__(self):
 		self.parser = reqparse.RequestParser()
