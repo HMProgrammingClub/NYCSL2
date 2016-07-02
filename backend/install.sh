c02a27810dcf2fc85c051c93529b077678152feb
Michael Truell <michael_truell@horacemann.org>
2016-07-02 14:54:51 -0400

    Switch to pip3

--- backend/install.sh
+++ backend/install.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 sudo apt-get update -y
+sudo apt-get install -y python3-pip
 
-yes | sudo apt-get install -y mongodb 
-sudo pip install -r requirements.txt
+yes | sudo apt-get install -y mongodb
+sudo pip3 install -r requirements.txt
