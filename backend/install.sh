e7aa5610c760dcf827f43bd41bc9cec3192e42fb
Michael Truell <michael_truell@horacemann.org>
2016-07-02 14:41:39 -0400

    feeling out travis

--- backend/install.sh
+++ backend/install.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 sudo apt-get update -y
 
-sudo apt-get install -y python3 python3-pip mongodb 
-sudo pip3 install -r requirements.txt || sudo pip install -r requirements.txt
+sudo apt-get install -y mongodb 
+sudo pip install -r requirements.txt
