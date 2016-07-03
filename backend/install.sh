a45eed3e99c00974815a6c2e0fed8bc57e9a6c6e
Michael Truell <michael_truell@horacemann.org>
2016-07-03 16:36:14 -0400

    Install newer mongodb version (to allow for text indexing)

--- backend/install.sh
+++ backend/install.sh
@@ -1,4 +1,8 @@
 #!/bin/bash
+
+apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv EA312927
+echo "deb http://repo.mongodb.org/apt/ubuntu trusty/mongodb-org/3.2 multiverse" | tee /etc/apt/sources.list.d/mongodb-org-3.2.list
 apt-get update -y
-apt-get --assume-yes -y install mongodb
+apt-get --assume-yes -y install mongodb-org
+
 pip3 install -r requirements.txt
