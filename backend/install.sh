0427db2a4734a9cb7a5c0f3fb0a545f8634733e9
Michael Truell <michael_truell@horacemann.org>
2016-07-03 15:11:33 -0400

    Fix bash try catch

--- backend/install.sh
+++ backend/install.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 apt-get update -y
-yes | apt-get install -y mongodb
-pip3 install -r requirements.txt | pip install -r requirements.txt
+apt-get install -y -qq mongodb
+pip3 install -r requirements.txt || pip install -r requirements.txt
