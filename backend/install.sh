4a3cf97a07b1624c10759cc4bf7c6f8ae7fd95a4
Michael Truell <truell20@gmail.com>
2016-08-25 22:07:55 -0400

    Add python and pip to install.sh

--- backend/install.sh
+++ backend/install.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+apt-get install -y python3 python3-pip
+
 apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv EA312927
 echo "deb http://repo.mongodb.org/apt/ubuntu trusty/mongodb-org/3.2 multiverse" | tee /etc/apt/sources.list.d/mongodb-org-3.2.list
 apt-get update -y
